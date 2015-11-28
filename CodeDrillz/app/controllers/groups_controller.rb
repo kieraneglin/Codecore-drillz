d1f8b3187189a3ac958a565b51109b71b2cd396c
Aaron Bui <aaronbui604@gmail.com>
2015-11-28 11:48:50 -0800

    added group controller

---
+++ CodeDrillz/app/controllers/groups_controller.rb
@@ -0,0 +1,62 @@
+class GroupsController < ApplicationController
+
+	before_action :find_group, {only: [:show, :edit, :update, :destroy]}
+ #  before_action :authenticate_user, except: [:index, :show]
+ #  before_action :authorize, only: [:edit, :update, :destroy]
+
+  def new
+    # authenticate_user 
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    # @group.user = current_user
+    if @group.save
+      redirect_to(group_path(@group))
+    else
+      render :new
+    end
+  end
+
+  def show
+    @drill = Drill.new
+  end
+
+  def edit
+    redirect_to groupss_path, alert: "Access Denied"
+  end
+
+  def update
+    if  @group.update(group_params)
+      redirect_to group_path(@group), notice: "group updated!" 
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @groups = Group.all
+  end
+
+  def destroy
+    @group.destroy
+    flash[:notice] = "group deleted!"
+    redirect_to groups_path
+  end
+
+  # def authorize
+  #   redirect_to groups_path, alert: "Access Denied" unless can? :manage, @q
+  # end
+
+  private
+
+  def find_group
+    @group = Group.find params[:id]
+  end
+
+  def group_params
+    params.require(:group).permit([:name, :description, :level, :badges, :points])
+  end
+
+end
--- CodeDrillz/app/models/group.rb
+++ CodeDrillz/app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+
+  
 end
