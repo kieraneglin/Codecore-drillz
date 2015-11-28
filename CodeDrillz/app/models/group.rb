605de707d20bbfb784b0a8af185aec3d9ee1b8ff
Kieran Eglin <kieran.eglin@gmail.com>
2015-11-28 14:50:18 -0800

    Cleaned up group and catagory models

--- CodeDrillz/app/models/group.rb
+++ CodeDrillz/app/models/group.rb
@@ -1,15 +1,11 @@
 class Group < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-<<<<<<< HEAD
-  
+
   has_many :drills, dependent: :destroy
   has_many :categories, dependent: :destroy
 
   validates :name, presence: true
   validates :level, presence: true
-=======
 
-  
->>>>>>> aaron_drillz
 end
