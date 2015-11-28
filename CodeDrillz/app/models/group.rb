81c1530cde6dd2bd69b2c37f759f136e5a5c8997
ivyr <olamit.ivy@gmail.com>
2015-11-28 14:54:13 -0800

    Add badge model related

--- CodeDrillz/app/models/group.rb
+++ CodeDrillz/app/models/group.rb
@@ -1,15 +1,14 @@
 class Group < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-<<<<<<< HEAD
-  
+
   has_many :drills, dependent: :destroy
   has_many :categories, dependent: :destroy
+  has_many :group_badges, dependent: :nullify
+  has_many :badges, through: :group_badges
+  has_many :earned_badges, dependent: :nullify
 
   validates :name, presence: true
   validates :level, presence: true
-=======
 
-  
->>>>>>> aaron_drillz
 end
