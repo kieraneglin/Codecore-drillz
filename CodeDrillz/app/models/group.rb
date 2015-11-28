1b708b54ffac8d6948f3460d6278315121ddcb8f
ivyr <olamit.ivy@gmail.com>
2015-11-28 10:32:36 -0800

    Add has_many relationship and validation

--- CodeDrillz/app/models/group.rb
+++ CodeDrillz/app/models/group.rb
@@ -1,4 +1,9 @@
 class Group < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+  has_many :drills, dependent: :destroy
+  has_many :categories, dependent: :destroy
+
+  validates :name, presence: true
+  validates :level, presence: true
 end
