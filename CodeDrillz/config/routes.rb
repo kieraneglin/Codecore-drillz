1a6a61a478447137c47eccf78ed51f9d41ed82b4
Kieran Eglin <kieran.eglin@gmail.com>
2015-11-29 12:33:05 -0800

    Updated routes

--- CodeDrillz/config/routes.rb
+++ CodeDrillz/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  # devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # devise_for :users
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
