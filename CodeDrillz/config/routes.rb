b3c17d11c320ca98b637f9bb34f193eb3234f29e
Paul Lucas <paulll.lucas@gmail.com>
2015-11-28 17:39:28 -0800

    integrated admin control panel and authentication

--- CodeDrillz/config/routes.rb
+++ CodeDrillz/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # devise_for :users
+  devise_for :users
+  # , ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
   resources :groups
   resources :categories
-  
+
 
 end
