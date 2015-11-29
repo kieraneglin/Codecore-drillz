cb3020d7e6639a379a54b7f1b97d6bc4e8976bd5
Kieran Eglin <kieran.eglin@gmail.com>
2015-11-29 09:37:14 -0800

    Fixed missing div tag and resolved double-migration for some collaborators

--- CodeDrillz/db/migrate/20151129070634_add_fields_to_users.rb
+++ CodeDrillz/db/migrate/20151129070634_add_fields_to_users.rb
@@ -1,8 +1,10 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :email, :string
-    add_column :users, :password_digest, :string
+    unless column_exists? :users, :first_name
+      add_column :users, :first_name, :string
+      add_column :users, :last_name, :string
+      add_column :users, :email, :string
+      add_column :users, :password_digest, :string
+    end
   end
 end
