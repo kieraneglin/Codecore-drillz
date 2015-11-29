1dac914d5fe66d6ef2309b4fa600850f0fe7f2c2
Paul Lucas <paulll.lucas@gmail.com>
2015-11-28 19:23:10 -0800

    rollback incase devise:views doesn't work

--- CodeDrillz/app/models/user.rb
+++ CodeDrillz/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :points, dependent: :destroy
   has_many :groups, dependent: :destroy
@@ -10,4 +10,26 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super # Use whatever other message
+    end
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+     recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+     if !recoverable.approved?
+       recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+     elsif recoverable.persisted?
+       recoverable.send_reset_password_instructions
+     end
+     recoverable
+   end
 end
--- CodeDrillz/db/schema.rb
+++ CodeDrillz/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129010050) do
+ActiveRecord::Schema.define(version: 20151129023301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 20151129010050) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.boolean  "approved"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
