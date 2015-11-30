3ec5c57e6fbfdea3d1e61ebe7a8ce2d86aa86755
ivyr <olamit.ivy@gmail.com>
2015-11-29 16:39:46 -0800

    Add user_drill model and association and add validation for solution and drill model

--- CodeDrillz/app/models/solution.rb
+++ CodeDrillz/app/models/solution.rb
@@ -1,6 +1,6 @@
 class Solution < ActiveRecord::Base
   belongs_to :drill
 
-  # validates :correct_answer, presence: true
-  # validates :type, presence: true
+  validates :correct_answer, presence: true
+  validates :solution_type, presence: true
 end
